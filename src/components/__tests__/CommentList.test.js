49ed1b9d2e0e37c99d22e63f1742fbca9c418657
Ivan <ivanvlora@gmail.com>
2019-04-21 20:56:51 +0900

    tested comment list

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -18,5 +18,10 @@ beforeEach(() => {
 });
 
 it("creates one LI per comments.", () => {
-  console.log(component.find("li").length);
+  expect(component.find("li").length).toEqual(2);
+});
+
+it("shows the text for each comment", () => {
+  expect(component.render().text()).toContain("Comment 1");
+  expect(component.render().text()).toContain("Comment 2");
 });
